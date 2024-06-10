dbf2afb34a75daa1fc6c69d520ad6c469ac6ec2f
LIM0310 <limsy0310@gmail.com>
2024-06-10 20:29:57 +0900

    수업내용실습

--- oz-django/boards/models.py
+++ oz-django/boards/models.py
@@ -9,6 +9,6 @@ class Board(CommonModel):
     date = models.DateTimeField(auto_now_add=True)
     likes = models.PositiveIntegerField(default=0)
     reviews = models.PositiveIntegerField(default=0)
-
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE) # 추가
     def __str__(self):
         return self.title
--- oz-django/users/views.py
+++ oz-django/users/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import MyInfoUserSerializer
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.authentication import TokenAuthentication # 사용자 인증 (추가)
+from rest_framework.permissions import IsAuthenticated # 권한 부여 (추가)
+class Users(APIView):
+    
+    def post(self, request):
+        password = request.data.get('password')
+        serializer = MyInfoUserSerializer(data = request.data)
+        try :
+            validate_password(password)
+        except :
+            raise Response(serializer.errors)
+        
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            
+            serializer = MyInfoUserSerializer(user)
+            return Response(serializer.data)
+        else :
+            raise Response(serializer.errors)
+        
+# api/v1/users/myinfo [GET,PUT]
+class MyInfo(APIView):
+    authentication_classes = [TokenAuthentication] # 추가
+    permission_classes = [IsAuthenticated] # 추가
+    # read
+    def get(self, request):
+        user = request.user
+        serializer = MyInfoUserSerializer(user)
+        return Response(serializer.data)
 
-# Create your views here.
+    # update
+    def put(self, request):
+        user = request.user
+        serializer = MyInfoUserSerializer(user, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            serializer = MyInfoUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
