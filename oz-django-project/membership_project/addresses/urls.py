1edf264444a05a45303fdc344c0c771dbf8e81ca
LIM0310 <limsy0310@gmail.com>
2024-06-11 20:55:53 +0900

    add challenge6

--- oz-django-project/membership_project/addresses/urls.py
+++ oz-django-project/membership_project/addresses/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('<int:pk>/update', views.UpdateAddress.as_view(), name='update-address'),
     path('<int:pk>/delete', views.DeleteAddress.as_view(), name='delete-address'),
     path("getToken", obtain_auth_token), # username,password를 보내면 토큰을 반환
+    path("/login/simpleJWT", TokenObtainPairView.as_view()),#코드 추가 부분
+    path("/login/simpleJWT/refresh", TokenRefreshView.as_view()),#코드 추가 부분
+    path("/login/simpleJWT/verify", TokenVerifyView.as_view())#코드 추가 부분
 ]
